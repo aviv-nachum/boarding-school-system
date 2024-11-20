66fa8ae4c97dbbc78c4ab4a667bf182ea66b0a59
BuilderOfOz <150072067+aviv-nachum@users.noreply.github.com>
2024-11-20 21:34:59 +0200

    notes from meeting

--- server-client-example.py
+++ server-client-example.py
@@ -3,12 +3,18 @@ from threading import Thread
 import threading
 import time
 from random import randint
+import struct # TODO: read about this
+
+# TODO: the client needs to send the message to the server and not other clients
+# TODO: replace list with dictionery where the key is the id and the value is the socket (maybe socket dosent need to save socket)
+# TODO: crate .py to server, client and for message
+# TODO: learn sirialization
 
 clients = []
 HOST = 'localhost'
 PORT = 8000
 
-def read_from_socket(conn):
+def read_from_socket(conn): # TODO: not one at a time, insted send the message length too
     """gets a port and tries to recive from it untill the message end with a '\n'"""
     buf = bytearray(0)
     while True:
@@ -17,7 +23,7 @@ def read_from_socket(conn):
         if b == b'\n':
                 return bytes(buf)
 
-class Server(threading.Thread):
+class Server(threading.Thread): # TODO: check out "threading.Lock"
     """server class, built to recive messages from client and transfer them to a diffrent client"""
     def __init__(self):
         super(Server, self).__init__()
@@ -40,18 +46,17 @@ class Server(threading.Thread):
              msg = read_from_socket(conn)
              id_str, _, msg = msg.partition(b':')
              clients[int(id_str)].sendall(msg)
-
-
-class Client(threading.Thread):
-    """client class built to send a message to another """
-    def sendMessage(self, message):
+ 
+class Client(threading.Thread): # dosent need to inherit from thread
+    """client class built to send a message to another client"""
+    def sendMessage(self, message, to):
         if len(clients)>1:
-                to = randint(0, len(clients) - 1)
+                to = randint(0, len(clients) - 1) # TODO: not random, sends to server only
                 message = ('%s:sent to %s\n' % (to, self.name)) + message
                 print(message)
                 self.ss.send(message.encode('utf-8'))
 
-    def receiveMessage(self):
+    def receiveMessage(self): # TODO: fix this to work with struct.pack
         while 1:
             reply=self.ss.recv(1024)
             if reply != '':
@@ -61,7 +66,7 @@ class Client(threading.Thread):
     def run(self):
         self.ss = socket(AF_INET, SOCK_STREAM)
         self.ss.connect((HOST, PORT)) # client-side, connects to a host
-        self.name=self.ss.getsockname()[1]
+        self.name=self.ss.getsockname()[1] # call this id, ids need to be diffrent 
 
         while True:
             self.receiveMessage()
